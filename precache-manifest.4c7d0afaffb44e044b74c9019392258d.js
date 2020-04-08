13ff807cc1eb4e883531adc606d4e5426b679a7e
IvanPustovit <pustovit.for@gmail.com>
2020-04-08 14:06:42 +0300

    Updates

---
+++ precache-manifest.4c7d0afaffb44e044b74c9019392258d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10f64983a0cc9f0196e182dba4c9202",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "68f1418b713f314fea87",
+    "url": "/my-app/static/js/2.8b7b2f30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.8b7b2f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b3ae78c302e688172d",
+    "url": "/my-app/static/js/main.a244e411.chunk.js"
+  },
+  {
+    "revision": "7d5358a866badb3f1584",
+    "url": "/my-app/static/js/runtime-main.07e30de1.js"
+  }
+]);
